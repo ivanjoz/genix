aca6c419cb03da24cc5a5705ebc2ed4864ee1333
ivanjoz <ivan@un.pe>
2024-04-03 23:32:38 -0500

    mejora buscador de productos venta

--- frontend/src/routes/comercial/ventas.tsx
+++ frontend/src/routes/comercial/ventas.tsx
@@ -1,15 +1,21 @@
-import { createEffect, createMemo, createSignal } from "solid-js";
+import { For, createEffect, createMemo, createSignal } from "solid-js";
 import { deviceType } from "~/app";
 import { CardsList } from "~/components/Cards";
-import { SearchSelect } from "~/components/SearchSelect";
-import { Loading, throttle } from "~/core/main";
+import { SearchSelect, makeHighlString } from "~/components/SearchSelect";
+import { Loading, include, throttle } from "~/core/main";
 import { PageContainer } from "~/core/page";
-import { IProductoStock, getProductosStock, useProductosAPI } from "~/services/operaciones/productos";
+import { IProducto, IProductoStock, getProductosStock, useProductosAPI } from "~/services/operaciones/productos";
 import { useSedesAlmacenesAPI } from "~/services/operaciones/sedes-almacenes";
 import { arrayToMapG, arrayToMapN, arrayToMapS, formatN } from "~/shared/main";
 import { Params } from "~/shared/security";
 import style from "./ventas.module.css";
 import { CheckBox } from "~/components/Input";
+
+export interface ProductoStock {
+  key: string
+  producto: IProducto
+  searchText: string
+}
 
 export default function Ventas() {
 
@@ -18,6 +24,7 @@ export default function Ventas() {
   const [almacenes] = useSedesAlmacenesAPI()
   const [form, setForm] = createSignal({ })
   const [filterText, setFilterText] = createSignal("")
+  const [productoSelected, setProductoSelected] = createSignal(-1)
 
   const getStock = async (almacenID: number) => {
     let stock: IProductoStock[] = []
@@ -32,20 +39,41 @@ export default function Ventas() {
     Params.setValue("almacen_id", almacenID)
     
     const productosStockMap = arrayToMapG(stock, "ProductoID")
-
-
     Loading.remove(); return
   }
 
-  const [productosParsed, setProdustosParsed] = createSignal([])
+  const [productosParsed, setProdustosParsed] = createSignal([] as ProductoStock[])
+  const [productosParsedAll, setProductosParsedAll] = createSignal([] as ProductoStock[])
 
   createEffect(() => {    
-      let newProductos = []
+      let newProductos: ProductoStock[] = []
       for(let i = 0; i < 40; i ++){
-        newProductos = newProductos.concat(productos()?.productos||[])
+        for(let e of productos()?.productos||[]){
+          newProductos.push({ 
+            key: `p_${i}_${e.ID}`, 
+            producto: e,
+            searchText: e.Nombre.toLowerCase()
+          })
+        }
       }
-      setProdustosParsed(newProductos)      
+      setProdustosParsed(newProductos) 
+      setProductosParsedAll(newProductos)        
   })
+
+  const filterProductos = (text: string) => {
+    if(text === filterText()){ return }
+    const textSlice = text.toLowerCase().split(" ")
+    const filtered = []
+    for(let e of productosParsedAll()){
+      if(include(e.searchText, textSlice)){
+        filtered.push(e)
+      }
+    }
+    console.log("filtrados::", filtered)
+    setProdustosParsed(filtered)
+    setFilterText(text)
+    setProductoSelected(-1)
+  }
   
   return <PageContainer title="Ventas" class="flex">
     <div class="jc-between mb-06" style={{ width: "46%" }}
@@ -61,12 +89,25 @@ export default function Ventas() {
         />
         <div class="search-c4 mr-16 w14rem">
           <div><i class="icon-search"></i></div>
-          <input class="w100" autocomplete="off" type="text" onKeyUp={ev => {
-            ev.stopPropagation()
-            throttle(() => {
-              setFilterText(((ev.target as any).value||"").toLowerCase().trim())
-            },150)
-          }}/>
+          <input class="w100" autocomplete="off" type="text" 
+            onKeyUp={ev => {
+              ev.stopPropagation()
+              throttle(() => {
+                const text = ((ev.target as any).value||"").toLowerCase().trim()
+                filterProductos(text)
+              },120)
+            }}
+            onkeydown={ev => {
+              ev.stopPropagation()
+              if(ev.key === 'ArrowUp'){
+                const newIdx = productoSelected()-1
+                if(newIdx < -1){ return }
+                setProductoSelected(newIdx)
+              } else if(ev.key === 'ArrowDown'){
+                setProductoSelected(productoSelected()+1)
+              }
+            }}
+          />
         </div>
       </div>
       <div class="flex jc-between w100">
@@ -76,16 +117,38 @@ export default function Ventas() {
       <div style={{ height: '70vh', position: 'relative' }}>
         <CardsList data={productosParsed()}
           render={(e,i) => {
+            const isSelected = () => { return i === productoSelected() }
+
             return <div class="px-04 py-02" 
-              style={{ "margin-top": i === 0 ? '8px' : undefined }}>
-              <div class={`flex jc-between ${style.card_venta}`}>
-                <div class="w100 grid" 
-                  style={{ "grid-template-columns": '1fr 3rem 5rem' }}>
-                  
-                  <div>{e.Nombre}  </div>
+              style={{ "margin-top": i === 0 ? '8px' : undefined }}
+              onmouseover={ev => {
+                ev.stopPropagation()
+                if(productoSelected() >= 0){ setProductoSelected(-1) }
+              }}
+            >
+              <div class={["flex p-rel jc-between",
+                  style.card_venta,
+                  isSelected() && style.card_venta_selected,
+                ].filter(x => x).join(" ")}
+                onClick={() => {
+                  setProductoSelected(i)
+                }}  
+              >
+                <div class={style.card_venta_nombre}>
+                  <div class={style.card_venta_nombre_line}></div>
+                    { makeHighlString(e.producto.Nombre, filterText()) }
+                </div>
+                <div class="w100 grid ai-center"
+                  style={{ "grid-template-columns": '1fr 3rem 5rem' }}>                  
+                  <div class={`flex ai-center h100`}>
+                    <div class={`${style.card_venta_producto}`}>
+                      { makeHighlString(e.producto.Nombre, filterText()) }
+                    </div>
+                    <ProductoCantidad />             
+                  </div>
                   <div class="ff-mono t-r">2</div>
                   <div class="ff-mono t-r">
-                    {formatN(e.Precio/100,2) as string}
+                    {formatN(e.producto.Precio/100,2) as string}
                   </div>
                 </div>
               </div>
@@ -98,4 +161,17 @@ export default function Ventas() {
       <h1>hola</h1>
     </div>
   </PageContainer>
+}
+
+function ProductoCantidad(){
+
+  const cantidades = [2,3,4,5,6,8,10,12]
+
+  return <div class={["flex ai-end h100",style.buttons_cantidad].join(" ")}>
+    <For each={cantidades}>
+      {e => {
+        return <div class={`flex-center ff-bold ${style.button_venta_cantidad}`}>{e}</div>
+      }} 
+    </For>
+  </div>
 }
