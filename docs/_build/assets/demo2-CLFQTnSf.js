8a28d0db269f579f27c8b2d2a9d444da3c089288
Ivan J. Angulo <ivan@un.pe>
2025-01-03 00:18:34 -0500

    deploy

--- docs/_build/assets/demo2-CLFQTnSf.js
+++
@@ -1,1 +0,0 @@
-import{c as t,t as e}from"./web-D2t8vN-4.js";import{P as r}from"./app-De9iPHfG.js";var a=e("<h1>Demo2");function n(){return t(r,{title:"Cajas",get children(){return a()}})}export{n as default};
