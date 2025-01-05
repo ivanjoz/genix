5fa335ce97f2ad8c6791260381278ff02477295a
Ivan J. Angulo <ivan@un.pe>
2025-01-05 15:06:08 -0500

    test prerender deploy

--- docs/_build/assets/demo2-hxoKruRv.js
+++
@@ -1,1 +0,0 @@
-import{c as t,t as e}from"./web-D7Y7EE6c.js";import{P as r}from"./app-DUJ9K0SH.js";var a=e("<h1>Demo2");function n(){return t(r,{title:"Cajas",get children(){return a()}})}export{n as default};
