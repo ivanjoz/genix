b748fdd5e8fe8d5d4ace6b98cdbcc620981cea11
Ivan Angulo <ivan@un.pe>
2025-11-18 21:18:17 -0500

    new version

---
+++ docs/_app/immutable/chunks/CA-Hm0sR.js
@@ -0,0 +1,1 @@
+import{n as a}from"./CoEcz8eb.js";a();
--- docs/_app/immutable/nodes/3.BuOhs02Y.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/DsnmJJEf.js";import"../chunks/CDoOrKf7.js";import{f as a,b as p}from"../chunks/xxzRbNoF.js";var t=a("<div><h1>hola</h1></div>");function v(o){var r=t();p(o,r)}export{v as component};
