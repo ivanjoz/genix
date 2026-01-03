a482f47282c1a87196c59b4063d7b8d206908db0
Ivan Angulo <ivan@un.pe>
2026-01-03 15:24:01 -0500

    improved ui

--- docs/_app/immutable/nodes/8.DgXzTlHi.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/DsnmJJEf.js";import"../chunks/BmzFTYzn.js";import{f as a,b as e}from"../chunks/Bf0lWsRu.js";import{P as m}from"../chunks/CA3qa6R_.js";var p=a("<h2>hola mundo</h2>");function h(o){m(o,{title:"Page Editor",children:(t,i)=>{var r=p();e(t,r)},$$slots:{default:!0}})}export{h as component};
