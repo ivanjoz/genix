b4d072ae8575b106a2d252ec9513205d65123770
Ivan Angulo <ivan@un.pe>
2026-04-12 21:46:35 -0500

    new version

--- frontend/static/sw.js
+++ frontend/static/sw.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff58ea933e2b0e4a311da3dd371582f74c77cdb85bf48e2d875494199e99e526
-size 308825
+oid sha256:5f742a2334b31c7e786fae8c3e1a8d620fe68c7fefd451d6d87387c944f48925
+size 124930
